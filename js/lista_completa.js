1b027376b889d79e4b6854b379a89374724b9aec
Santiago Torres <sanlbdp24@gmail.com>
2023-05-11 19:16:35 -0300

    Commit intentando fetch en lista completa

--- js/lista_completa.js
+++ js/lista_completa.js
@@ -26,7 +26,23 @@ function crearCarta(arrayPokemon){
 
 cargarCartas();
 
+const btnFiltroPorTipo = document.querySelectorAll(".nav-btn");
 
+btnFiltroPorTipo.forEach((button) => button.addEventListener("click", (e) => {
+    let listaClases = e.currentTarget.classList;
+    
+    for (let i=1; i<151; i++){
+        fetch(direccionPokeapi + i)
+            .then( (result) => result.json())
+            .then( (arrayPokemon) => {
+                const arrayTipos = arrayPokemon.types.map(type => type.type.name);
+                console.log(arrayTipos); //me da un array de cada tipo
+                
+                if (arrayTipos.includes())
+            });
+    }
+
+}));
 
 
 
