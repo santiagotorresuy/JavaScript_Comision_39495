a854cd5657bfc69b7e1bdadd6613b6ccbc8868f5
Santiago Torres <sanlbdp24@gmail.com>
2023-05-15 00:18:36 -0300

    Commit agregando funcionalidad q los botones filtro de la tienda pokemon

--- js/tiendaPokemon.js
+++ js/tiendaPokemon.js
@@ -1,13 +1,38 @@
+//ELEMENTOS DEL DOM
 const containerCartas = document.querySelector("#containerCartas");
+const btnFiltroCategoria = document.querySelectorAll(".nav-tienda-ul-li");
 
+
+//ARRAYS
 let  objetosPokemonCartas = [];
 
-function traerObjetos(data){
-    data.forEach((objeto) => objetosPokemonCartas.push(objeto))
+//FUNCIONES
 
+function cuerpoCarta(lista){
     containerCartas.innerHTML = ``;
 
-    for(let i=0; i <= 51; i++){
+    for(let i=0; i <= lista.length; i++){
+        const div = document.createElement("div");
+        div.classList.add("carta-producto");
+    
+        div.innerHTML = `
+            <p class="titulo-carta">${lista[i].nombre}</p>
+            <img src="${lista[i].img}" alt="">
+            <p class="carta-precio">${lista[i].precio} ¥</p>
+            <form action="" class="form-agregar-carrito">
+                <input type="text" class="input-agregar-carrito">
+                <button class="btn-agregar-carrito">Agregar al carrito</button>
+            </form>
+        `;containerCartas.appendChild(div);
+    }
+}
+
+function crearCartasObjetos(data){
+    data.forEach((objeto) => objetosPokemonCartas.push(objeto))
+    cuerpoCarta(objetosPokemonCartas)
+    /*containerCartas.innerHTML = ``;
+
+    for(let i=0; i <= objetosPokemonCartas.length; i++){
         const div = document.createElement("div");
         div.classList.add("carta-producto");
     
@@ -20,13 +45,38 @@ function traerObjetos(data){
                 <button class="btn-agregar-carrito">Agregar al carrito</button>
             </form>
         `;containerCartas.appendChild(div);
-    }
+    }*/
 };
 
-function mostrarObjetos(){
-    fetch("../objetosPokemon.json")
-        .then( (Response) => Response.json())
-        .then( (objetosPokemon) => traerObjetos(objetosPokemon))
+async function mostrarObjetos(){
+    objetosPokemonCartas = await fetch("../objetosPokemon.json")
+        .then((response) => {
+            if (response.ok){
+                return response.json();
+            }
+        })
+        .then((objetos) => crearCartasObjetos(objetos))  
 };
 
-mostrarObjetos();
+function filtrarObjetos (){
+    btnFiltroCategoria.forEach((btn) => btn.addEventListener("click", (e) =>{
+        const btnId = e.currentTarget.id;
+        containerCartas.innerHTML = ``
+        
+        if(btnId === "todos"){
+            mostrarObjetos()
+        }else{
+            const objetosFiltrados = objetosPokemonCartas.filter((objeto) => objeto.categoria === btnId)
+            cuerpoCarta(objetosFiltrados);
+        }
+    }));
+}
+
+//CODIGO
+
+document.addEventListener("DOMContentLoaded", () =>{
+    mostrarObjetos();
+    filtrarObjetos();
+})
+
+
